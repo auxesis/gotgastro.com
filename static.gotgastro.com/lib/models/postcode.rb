8720e58267ef65d65d12f5f3da1b53b318c9ccf8
Lindsay Holmwood <lindsay@holmwood.id.au>
2009-11-13 15:30:43 +0100

    added finder to get aggregated counts

--- static.gotgastro.com/lib/models/postcode.rb
+++ static.gotgastro.com/lib/models/postcode.rb
@@ -12,4 +12,13 @@ class Postcode
     attribute_set(:id, value)
   end
 
+  def self.top_ten
+    results = repository(:default).adapter.query("SELECT notices.postcode, COUNT(notices.postcode), total_businesses FROM notices INNER JOIN postcodes ON postcodes.id = notices.postcode GROUP BY postcode ORDER BY COUNT(notices.postcode);")
+    results.map do |r| 
+      { 'postcode' => Postcode.get(r[:"notices.postcode"]), 
+        'count' => r[:"coun_t(notices.postcode)"], 
+        'total' => r["total_businesses"] }
+    end
+  end
+
 end
