c1486c1f36eb03c72d2415f21e026b17bd8c38a7
Lindsay Holmwood <lindsay@holmwood.id.au>
2009-11-13 02:03:53 +0100

    verify different types of json produced. verify validity of source files

--- abs-business-counts/features/steps/processor_steps.rb
+++ abs-business-counts/features/steps/processor_steps.rb
@@ -18,14 +18,29 @@ Given /^I have a file ending with "(.+)" in "([^\"]*)"$/ do |extension, director
   Dir.glob("#{directory}/abs-8165009#{extension}").size.should > 0
 end
 
-Then /^the JSON in "([^\"]*)" should have lat\/lng co\-ordinates$/ do |directory|
-  filename = "#{directory}/abs-8165009-normalised.json"
+Then /^the (\w*\s*)JSON in "([^\"]*)" should have an "([^\"]*)" attribute on every entry$/ do |type, directory, attribute|
+  filename = "#{directory}/abs-8165009#{type.blank? ? '' : "-#{type.strip}"}.json"
+  File.exists?(filename).should be_true
+  
+  json = File.new(filename, 'r')
+  parser = Yajl::Parser.new
+  data = parser.parse(json)
+
+  data.each do |entry|
+    entry[attribute].should_not be_nil
+  end
+end
+
+Given /^I have an sla2poa mapping file at "([^\"]*)"$/ do |filename|
   File.exists?(filename).should be_true
 
   json = File.new(filename, 'r')
   parser = Yajl::Parser.new
   data = parser.parse(json)
 
-  data.detect { |entry| entry['lat'] || entry['lng'] }.should_not be_nil
+  data.each do |entry|
+    entry['sla_name'].should_not be_nil
+    entry['sla_code'].should_not be_nil
+    entry['poa_code'].should_not be_nil
+  end
 end
-
