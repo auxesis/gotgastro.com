b9c2af7a589f435b1b41be2557dc39b00868ed60
Lindsay Holmwood <lindsay@holmwood.id.au>
2009-11-13 17:20:10 +0100

    require dm-validations

--- static.gotgastro.com/lib/datamapper.rb
+++ static.gotgastro.com/lib/datamapper.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 Gem.path << File.expand_path(File.join(File.dirname(__FILE__), '..', 'gems'))
 
 require 'dm-core'
+require 'dm-validations'
 
 DataMapper.setup(:default, "sqlite3://#{File.expand_path(File.join(File.dirname(__FILE__), '..', 'gastro.db'))}")
 
