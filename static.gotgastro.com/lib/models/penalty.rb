b740c664290229a499e30ccdaf64043d69052981
Lindsay Holmwood <lindsay@holmwood.id.au>
2010-01-31 15:09:41 +0100

    return nil more readily when the data is blank

--- static.gotgastro.com/lib/models/penalty.rb
+++ static.gotgastro.com/lib/models/penalty.rb
@@ -4,8 +4,9 @@ class Penalty < Notice
   # these are available on Prosecutions also
   instance_eval do                                                                                               
     [:trading_name, :address, :served_to, :council_area].each do |method|                                        
-      define_method method do                                                                                    
-        self.instance_variable_get(:"@#{method.to_s}").downcase.gsub(/^[a-z]|\s+[a-z]/) { |a| a.upcase }
+      define_method method do                           
+        value = self.instance_variable_get(:"@#{method.to_s}")
+        value ? value.downcase.gsub(/^[a-z]|\s+[a-z]/) { |a| a.upcase } : nil
       end                                                                                                        
     end                                                                                                          
   end  
